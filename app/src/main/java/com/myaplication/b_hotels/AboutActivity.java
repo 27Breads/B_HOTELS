3be747c50ec969580783ac0fa37f1d25ef31483b
airfandi <ekodann@gmail.com>
2020-11-29 22:39:31 +0700

    tambah main activity, about activity, dan spalshscreen activity

---
+++ app/src/main/java/com/myaplication/b_hotels/AboutActivity.java
@@ -0,0 +1,25 @@
+package com.myaplication.b_hotels;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finishAffinity();
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+    }
+}
